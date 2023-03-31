a7f8239bf934a32120d0f1949fc0a1331b107f34
Brad Beshaw <Brad@Beshaw.com>
2023-03-31 14:46:20 -0400

    init

---
+++ data_connect.py
@@ -0,0 +1,8 @@
+
+def GetConnectionString():
+    connectionString = 'DRIVER={SQL Server};SERVER=192.168.0.84;PORT=1433;DATABASE=PLCData;UID=nbty;PWD=nbty;'
+    return connectionString
+
+def GetRootString():
+    rootString = '<h1 class="display-4">Welcome to Elemco Software Web Services.</h1>'
+    return rootString
---
+++ shopfloorapi.py
@@ -0,0 +1,51 @@
+import json
+import pyodbc
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import data_connect
+import requests
+app = Flask(__name__)
+app.app_context().push()
+if(__name__=="__main__"):
+    app.run()
+#------------------------
+connectionString = data_connect.GetConnectionString()
+conn = pyodbc.connect(connectionString)
+#------------------------
+sqlstring = 'SELECT * FROM [PLCData].[dbo].[PLC_Master]'
+cursor = conn.cursor()
+@app.route('/')
+def index():
+    rootString = data_connect.GetRootString()
+    return rootString
+@app.route('/plcinfo/', methods=['GET'])
+def get_plcinfo():
+    sqlstring = 'SELECT * FROM [PLCData].[dbo].[PLC_Master]'
+    plcinfo = []
+    output = '{"PLC_Master":'
+    cursor.execute(sqlstring)
+    for row in cursor:
+        plcstring = 'Id: ' + str(row.id) + ', PLC_Address: ' + row.PLC_Address + ', PLC_Name: ' + row.PLC_Name
+        plcinfo.append(plcstring)
+    
+    output += json.dumps(plcinfo)
+    output += "}"
+    return output
+@app.route('/plcinfo/<plcid>', methods=['GET'])
+def get_plcid(plcid):
+    sqlstring = 'SELECT * FROM [PLCData].[dbo].[PLC_Master] Where [id] = ' + plcid
+    plcinfo = []
+    output = '{"PLC_Master":'
+    cursor.execute(sqlstring)
+    found = 0
+    for row in cursor:
+        found = 1
+        plcstring = 'Id: ' + str(row.id) + ', PLC_Address: ' + row.PLC_Address + ', PLC_Name: ' + row.PLC_Name
+        plcinfo.append(plcstring)
+
+    if found == 1:
+        output += json.dumps(plcinfo)
+        output += "}"
+        return output
+    else:
+        output = "There was no information found for ID " + plcid
